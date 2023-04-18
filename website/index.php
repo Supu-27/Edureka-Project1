4849d18dce2b3fe999c41d9300f5160c037fdf65
Supriya <125067454+Supu-27@users.noreply.github.com>
2023-04-18 07:12:49 -0700

    Update index.php

--- website/index.php
+++ website/index.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "Hello world!";
+echo "https://www.bing.com/ck/a?!&&p=64012a517c3381fbJmltdHM9MTY4MTc3NjAwMCZpZ3VpZD0yNDYyZjIwYS03ZTBjLTZjM2YtMWM2OS1lMGZkN2ZmZTZkYzgmaW5zaWQ9NTQ4NA&ptn=3&hsh=3&fclid=2462f20a-7e0c-6c3f-1c69-e0fd7ffe6dc8&psq=phpmyadmin+download&u=a1aHR0cHM6Ly93d3cucGhwbXlhZG1pbi5uZXQvZG93bmxvYWRzLw&ntb=1";
 
 
 require 'config.php';
